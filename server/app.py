a74587cbebabd97da5f1160c15a5f0059f3d7865
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-21 21:44:44 +1000

    complete lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,33 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:para>')
+def print_string(para):
+    print(para)
+    return f'{para}'
+
+@app.route('/count/<int:para>')
+def count(para):
+    nums = '\n'.join(str(num) for num in range(0, para))
+    return f'{nums}\n'
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1+num2)
+    elif operation == '-':
+        return str(num1-num2)
+    elif operation == '*':
+        return str(num1*num2)
+    elif operation == 'div':
+        return str(num1/num2)
+    elif operation == '%':
+        return str(num1%num2)
+    else:
+        return 'Please use +, -, *, div or %'
+        
